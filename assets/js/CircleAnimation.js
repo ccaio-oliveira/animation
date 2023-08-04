1fd933c78306acdb098cbde6fbb651957ba37925
Vinicius Simonton Arcanjo da Silva - VAR <vinicius.arcanjo@iteris.com.br>
2023-08-04 15:09:34 -0300

    feat: adição do circulo na primeira dobra

---
+++ assets/js/CircleAnimation.js
@@ -0,0 +1,11 @@
+const circles = document.querySelectorAll(".circle")
+
+document.addEventListener("scroll", () => {
+  const scrollY = window.scrollY
+
+  for (index = 1; index <= circles.length; index++) {
+    const circle = circles[index - 1]
+    circle.style.transform = `rotate(${(scrollY * 0.5) / index}deg)`
+    circle.style.opacity = `${scrollY * 0.3}%`
+  }
+})
