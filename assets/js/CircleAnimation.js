6e3e6db1a2a993e8d3e0cdbdb59007c5d9a538a4
Vinicius Simonton Arcanjo da Silva - VAR <vinicius.arcanjo@iteris.com.br>
2023-08-05 13:06:22 -0300

    fix: css selectors

--- assets/js/CircleAnimation.js
+++ assets/js/CircleAnimation.js
@@ -1,11 +1,29 @@
 const circles = document.querySelectorAll(".circle")
 
+document.addEventListener("DOMContentLoaded", () => {
+  circles.forEach(circle => {
+    circle.style.opacity = 0
+  })
+})
+
 document.addEventListener("scroll", () => {
   const scrollY = window.scrollY
 
-  for (index = 1; index <= circles.length; index++) {
+  for (let index = 1; index <= 12; index++) {
     const circle = circles[index - 1]
-    circle.style.transform = `rotate(${(scrollY * 0.5) / index}deg)`
+
+    if (index <= 4) {
+      circle.style.transform = `rotate(${(scrollY * 0.5) / index}deg)`
+    }
+
+    if (index > 4 && index <= 8) {
+      circle.style.transform = `rotate(${(scrollY * 0.5) / (index - 4)}deg)`
+    }
+
+    if (index > 8) {
+      circle.style.transform = `rotate(${(scrollY * 0.5) / (index - 8)}deg)`
+    }
+
     circle.style.opacity = `${scrollY * 0.3}%`
   }
 })
